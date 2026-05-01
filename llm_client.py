d25dc1385f3f9a761caf34f6b54c341549a1448b
Arman Nurlanbek <arman.m.nurlanbek@gmail.com>
2026-05-01 16:41:53 +0500

    tools and OpenAI SDK

--- llm_client.py
+++ llm_client.py
@@ -6,13 +6,54 @@ from openai.types.responses import response
 
 load_dotenv()
 
-client = OpenAI()
-def chat(messages, tools=None) -> response:
-    response = client.responses.create(
-        model = "gpt-5.4",
-        tools = tools,
-        input = messages,
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+tools = [
+    {
+        "type": "function",
+        "name": "search_movie",
+        "description": "Search for a movie by title",
+        "parameters": {
+            "type": "object",
+            "properties": {"title": {"type": "string"}},
+            "required": ["title"]
+        }
+    }
+]
+
+def search_movie(title: str):
+    return f"{title}: one of the best movies of all time maaan"
+
+input_list = [
+    {"role": "user", "content": "Search up the titanic movie using search_movie tool"}
+]
+
+
+response = client.responses.create(
+    model = "gpt-4o-mini",   
+    instructions = "You are a movie expert. You are given a question about movies and you need to answer it.",   
+    input = input_list,
+    tools = tools, 
     )
 
-    return response.output_text
+input_list += response.output
+
+for item in response.output:
+    if item == "function_call":
+        if item.name == "search_movie":
+            title = json.loads(item.arguments)["sign"]
+            movie = search_movie(title)
+
+            input_list.append({
+                "type": "function_call_output",
+                "call_id": item.call_id,
+                "output": movie,
+            })
+
+print("Final output: ")
+print(input_list)
+print(response.model_dump_json(indent=2))
+print(response.output_text)
+
+
 
--- tools.py
+++ tools.py
@@ -2,8 +2,37 @@
 from agents import function_tool
 
 
-@function_tool
+tools = [
+    {
+        "type": "function",
+        "name": "search_movie",
+        "description": "Search for a movie by title",
+        "parameters": {
+            "type": "object",
+            "properties": {"title": {"type": "string"}},
+            "required": ["title"]
+        }
+    }
+]
+
 def search_movie(title: str):
+    return f"{title}: one of the best movies of all time maaan"
+
+
+@function_tool
+def search_movie_list(query: str):
     return None
+
+@function_tool
+def compare_movies(title1: str, title2: str):
+    return None
+
+@function_tool
+def get_movies_by_genre(genre: str):
+    return None
+
+@function_tool
+def filter_by_rating(min_rating:int):
+    return Non
 
 
